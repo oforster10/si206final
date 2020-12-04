eea778e30ca375bf16f8c8f256dd608eee866d9d
oforster10 <oforster@umich.edu>
2020-12-03 22:10:44 -0500

    Inserted data into Table

--- gathering_spotify_data.py
+++ gathering_spotify_data.py
@@ -22,8 +22,8 @@ def setUpDatabase(db_name):
     cur = conn.cursor()
     return cur, conn
 
-def get_spotify_top_tracks_of_2020():
-    """No inputs. Returns a list of tuples in the format (song, artist) using playlist 'Top Tracks of 2020 USA'."""
+def get_spotify_most_streamed():
+    """No inputs. Returns a list of tuples in the format (song, artist, popularity) using playlist 'Top 100 Most Streamed Songs on Spotify'."""
     playlist_id = "5ABHKGoOzxkaa28ttQV9sE"
     playlist = sp.playlist(playlist_id, fields=None, market=None, additional_types=('track', ))
     tracks = playlist['tracks']['items']
@@ -31,46 +31,60 @@ def get_spotify_top_tracks_of_2020():
     for item in tracks:
         title = item['track']['name']
         artist = item['track']['album']['artists'][0]['name']
-        info = title, artist
+        popularity = int(item['track']['popularity'])
+        info = title, artist, popularity
         tracks_info.append(info)
     print(tracks_info)
-
-def get_song_popularity(song, artist):
-    """Take song and artist from the previous function as inputs. Go to artist's page on Spotify, find the matching song title, and return a tuple of the (song, artist, number_of_streams)."""
+    return tracks_info
 
 def set_up_tables(cur, conn):
     """ Takes the database cursor and connection as inputs. Returns nothing. Creates two tables, one that will hold artists and their artist_ids, and another that holds the top 100 songs, along with their artist_ids and weeks on chart."""
     #ask if we need to create both tables, unsure about SongIds table
     #cur.execute("CREATE TABLE IF NOT EXISTS SongIds (song_id INTEGER PRIMARY KEY, song TEXT, artist TEXT)")
-    cur.execute("CREATE TABLE IF NOT EXISTS Spotify (creation_id INTEGER PRIMARY KEY, artist TEXT, song_id INTEGER, number_of_streams INTEGER)")
+    cur.execute("CREATE TABLE IF NOT EXISTS Spotify (song_id INTEGER PRIMARY KEY, song TEXT, artist TEXT, popularity INTEGER)")
     conn.commit()
 
 def fill_spotify_table(cur, conn):
 # """Takes the database cursor and connection as inputs. Does not return anything. Fills in the Spotify table with songs and their artist_ids and the number of streams each song has. The creation_id is each artist's unique identification number."""
     #Calls get_spotify_top_tracks_of_2020() to get the songs off of the Spotify "Top Tracks of 2020 USA" playlist.
-    song, artist = get_spotify_top_tracks_of_2020()
-    spotify_list = get_song_popularity(song, artist)
-    #Selects songs that are already in the Spotify table and puts them in a list so that we know which songs should not be repeated.
-    cur.execute('SELECT song FROM SongIds')
+    most_streamed = get_spotify_most_streamed()
+
+    cur.execute("SELECT song FROM Spotify")
     song_list = cur.fetchall()
     x = 1
     count = len(song_list)
-    #Adds songs to the Spotify list that are not already in it, 25 at a time.
     for x in range(25):
         x = count
-        #The creation_id is the unique id that identifies a song.
-        creation_id = count + 1
-        song = spotify_list[count][0]
-        streams = spotify_list[count][2]
-        x = x + 1
-        #In order to save storage space, we are adding in ArtistIds (integers) instead of artists.
-        cur.execute('SELECT artist_id, artist FROM ArtistIds')
-        artist_ids = cur.fetchall()
-        for artist_tup in artist_ids:
-            if artist_tup[1] == top_100_list[count][1]:
-                artist = artist_tup[0]
-                cur.execute("INSERT OR IGNORE INTO Hot100 (creation_id, song, artist_id, weeks_on_chart) VALUES (?, ?, ?, ?)", (creation_id, song, int(artist), weeks))
-        count = count + 1
+        song_id = count + 1
+        song = most_streamed[count][0]
+        artist = most_streamed[count][1]
+        popularity = most_streamed[count][2]
+        x += 1
+        cur.execute("INSERT OR IGNORE INTO Spotify (song_id, song, artist, popularity) VALUES (?, ?, ?, ?)", (song_id, song, artist, popularity))
+        count += 1
+   
     conn.commit()
+    
+    # #Adds songs to the Spotify list that are not already in it, 25 at a time.
+    # for x in range(25):
+    #     x = count
+    #     #The creation_id is the unique id that identifies a song.
+    #     creation_id = count + 1
+    #     song = spotify_list[count][0]
+    #     streams = spotify_list[count][2]
+    #     x = x + 1
+    #     #In order to save storage space, we are adding in ArtistIds (integers) instead of artists.
+    #     cur.execute('SELECT artist_id, artist FROM ArtistIds')
+    #     artist_ids = cur.fetchall()
+    #     for artist_tup in artist_ids:
+    #         if artist_tup[1] == top_100_list[count][1]:
+    #             artist = artist_tup[0]
+    #             cur.execute("INSERT OR IGNORE INTO Hot100 (creation_id, song, artist_id, weeks_on_chart) VALUES (?, ?, ?, ?)", (creation_id, song, int(artist), weeks))
+    #     count = count + 1
+    # conn.commit()
 
-get_spotify_top_tracks_of_2020()
+get_spotify_most_streamed()
+
+cur, conn = setUpDatabase('spotify.db')
+set_up_tables(cur, conn)
+fill_spotify_table(cur, conn)
