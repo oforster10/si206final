e3c2f0b83a911641cdb503bf6e88e9a95dbc3a5e
oforster10 <oforster@umich.edu>
2020-12-07 13:06:46 -0500

    Finalized

--- calculations.py
+++ calculations.py
@@ -18,10 +18,10 @@ def artist_percent_of_top_100_songs(cur, conn):
         artist = song[0]
         artist_dict[artist] = artist_dict.get(artist, 0) + 1
      
-    for artist_count in artist_dict:
-        total_songs = artist_dict[artist_count]
-        cur.execute("UPDATE Artists SET percent_spotifytop100 = (?)", (total_songs,))
-        conn.commit()
+    for artist_name in artist_dict:
+        total_songs = artist_dict[artist_name]
+        # cur.execute("INSERT INTO ArtistsS VALUES (?, ?)", (artist_name, total_songs))
+        # conn.commit()
     return artist_dict
     
     
@@ -39,12 +39,13 @@ def artist_percent_of_total_views(cur, conn):
     
     percent_views = 0
     artist_percent_views = {}
-    for artist in views_dict:
-        percent_views = views_dict[artist] / total_views * 100
+    for artist_name in views_dict:
+        print(artist_name)
+        percent_views = views_dict[artist_name] / total_views * 100
         percent_views_float = "{:.2f}".format(percent_views)
-        artist_percent_views[artist] = percent_views_float
-        cur.execute("INSERT OR IGNORE INTO Artists (artist, percent_youtubeviews) VALUES (?, ?)", (artist, percent_views_float))
-        conn.commit()
+        artist_percent_views[artist_name] = percent_views_float
+        # cur.execute("INSERT INTO ArtistsY (artist, percent_youtubeviews) VALUES (?, ?)", (artist, percent_views_float))
+        # conn.commit()
     return artist_percent_views
 
 def song_percent_of_top_song_views(cur, conn):
@@ -59,9 +60,9 @@ def song_percent_of_top_song_views(cur, conn):
         song_name = song[3]
         youtube_popularity = "{:.2f}".format(views / top_song_views * 100)
         popularity_scores = (spotify_popularity, float(youtube_popularity))
-        cur.execute("INSERT INTO Songs (song, spotify_popularity_score, youtube_popularity_score) VALUES (?, ?, ?)", (song_name, spotify_popularity, youtube_popularity))
-        conn.commit()
-        song_popularity_dict[song] = popularity_scores
+        # cur.execute("INSERT INTO Songs (song, spotify_popularity_score, youtube_popularity_score) VALUES (?, ?, ?)", (song_name, spotify_popularity, youtube_popularity))
+        # conn.commit()
+        song_popularity_dict[song_name] = popularity_scores
     return song_popularity_dict
 
 
@@ -71,14 +72,24 @@ def write_data_to_file(filename, cur, conn):
     path = os.path.dirname(os.path.abspath(__file__)) + os.sep
 
     outFile = open(path + filename, "w")
-    outFile.write("Spotify and YouTube Data Calculations\n")
-    outFile.write("=============================================================\n\n")
+    
+    outFile.write("Popularity Scores of Songs on YouTube and Spotify\n")
+    outFile.write("================================================================================================\n\n")
     output1 = (song_percent_of_top_song_views(cur, conn))
     for song in output1:
-        outFile.write("For " + song + ", the Spotify popularity score is " + str(output1[song][0]) + " and the popularity for the corresponding song's music video on YouTube is " + str(output1[song][1]) + ".\n\n")
+        spotify_pop = str(output1[song][0])
+        outFile.write("For " + song + ", the Spotify popularity score is " + spotify_pop + " and the popularity for the corresponding song's music video on YouTube is " + str(output1[song][1]) + ".\n\n")
+   
+    outFile.write("================================================================================================\n")
+    outFile.write("Artist Percent of YouTube Music Video Views Corresponding to their Top 100 Spotify Songs\n")
+    outFile.write("================================================================================================\n")
     output2 = artist_percent_of_total_views(cur, conn)
     for artist in output2:
         outFile.write("Out of the top 100 Spotify songs, " + artist + "'s corresponding music videos have " + str(output2[artist]) + " percent of the total YouTube views for said Spotify songs." + "\n\n")
+    
+    outFile.write("================================================================================================\n")
+    outFile.write("Artist Percent of Top 100 Most Streamed Songs on Spotify\n")
+    outFile.write("================================================================================================\n")
     output3 = artist_percent_of_top_100_songs(cur, conn)
     for artist in output3:
         outFile.write(artist + " has written " + str(output3[artist]) + "%" +  " of the top 100 Most Streamed Songs on Spotify." + "\n\n")
@@ -86,14 +97,15 @@ def write_data_to_file(filename, cur, conn):
     outFile.close()
 
 path = os.path.dirname(os.path.abspath(__file__))
-conn = sqlite3.connect(path+'/'+'spotify.db')
+conn = sqlite3.connect(path+'/'+'music.db')
 cur = conn.cursor()
 
-cur.execute("DROP TABLE Artists")
-cur.execute("CREATE TABLE IF NOT EXISTS Artists (artist TEXT PRIMARY KEY, percent_spotifytop100 INTEGER, percent_youtubeviews INTEGER)")
+cur.execute("CREATE TABLE IF NOT EXISTS ArtistsS (artist TEXT PRIMARY KEY, percent_spotifytop100 INTEGER)")
+cur.execute("CREATE TABLE IF NOT EXISTS ArtistsY (artist TEXT PRIMARY KEY, percent_youtubeviews INTEGER)")
 cur.execute("CREATE TABLE IF NOT EXISTS Songs (song TEXT PRIMARY KEY, spotify_popularity_score INTEGER, youtube_popularity_score INTEGER)")
 
-artist_percent_of_total_views(cur, conn)
-artist_percent_of_top_100_songs(cur, conn)
-song_percent_of_top_song_views(cur, conn)
-# write_data_to_file("calculations", cur, conn)
+# song_percent_of_top_song_views(cur, conn)
+# artist_percent_of_total_views(cur, conn)
+
+# artist_percent_of_top_100_songs(cur, conn)
+write_data_to_file("calculations", cur, conn)
